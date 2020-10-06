5ec0326c76f6020d38c1959fa472ff28c9a5f16c
Petrik Lajos Szg <info@petrik.hu>
2020-10-06 10:46:43 +0200

    karakter osztály

--- DnD/Karakter.cs
+++ DnD/Karakter.cs
@@ -8,6 +8,19 @@ namespace KalandJatek
 {
     class Karakter
     {
-        int a;
+        string nev;
+        int eletero;
+        int sebzes;
+
+        public Karakter(string nev, int eletero, int sebzes)
+        {
+            this.nev = nev;
+            this.eletero = eletero;
+            this.sebzes = sebzes;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public int Eletero { get => eletero; set => eletero = value; }
+        public int Sebzes { get => sebzes; set => sebzes = value; }
     }
 }
